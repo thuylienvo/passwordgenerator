44e80653378f5d76f06b57e5a56ea0e693916b89
Mimi Vo <mimzy414@gmail.com>
2021-04-08 14:28:50 -0600

    added pw random gen code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,13 @@
  const alphabetEl = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z'];
  const specialCharEl = ['!', '@', '#', '?', '$', '*', '^'];
 
-// Prompts after user clicks Generate Password
+// Generate Password Prompts
 // ================================================================
 var generateBtn = document.querySelector("#generate");
    
 // Prompt for length of password between 8 and 128 characters
 function capturePwLength() {
-    var pwLength = window.prompt("Please enter the number of characters you would like for your password. Must between 8 and 128. Must have at least 1 character selection.")
+    var pwLength = window.prompt("Please enter the number of characters you would like for your password. Must between 8 and 128.")
     console.log(pwLength)
     if(pwLength % 1 != 0 || pwLength <8 || pwLength >128) {
         window.alert("Error: You must input a whole number between 8 and 128.")
@@ -17,16 +17,18 @@ function capturePwLength() {
     return pwLength
 }
 
-//Prompt for character property types 
+// Prompt for character property types 
 function capturePwCharType() { 
+    window.alert("Please select the password character criteria. Must select at least 1.")
+
     //var pwCharType = [upperCases, lowerCases, numbers, special] 
     var upperCases = confirm("Do you want uppercase letters in your password?")
     console.log(upperCases)
-    var lowerCases = confirm("Do you want lowercase letters in your password??")
+    var lowerCases = confirm("Do you want lowercase letters in your password?")
     console.log(lowerCases)
     var numbers = confirm("Do you want numbers in your password?")
     console.log(numbers)
-    var special = confirm("Do you want special characters in your password??")
+    var special = confirm("Do you want special characters in your password?")
     console.log(special)
     var pwCharType = [upperCases, lowerCases, numbers, special] 
     if(upperCases == false && lowerCases == false && numbers == false && special ==false) {
@@ -45,6 +47,33 @@ function generatePassword() {
 
     // User input character properties
     var pwCharEl = capturePwCharType();
+    var upperC = pwCharEl[0]
+    var lowerC = pwCharEl[1]
+    var numbC = pwCharEl[2]
+    var specialC = pwCharEl[3]
+
+    // Randomly generate characters
+    if( upperC === true) {
+        var upper = alphabetEl[Math.floor(Math.random() * alphabetEl.length)].toUpperCase()
+        console.log("The uppercase selected is: " + upper)
+    };
+    if( lowerC === true) {
+        var lower = alphabetEl[Math.floor(Math.random() * alphabetEl.length)]
+        console.log("The lowercase selected is: " + lower)
+    };
+    if( numbC === true) {
+        var numb = Math.floor(Math.random() * 9)
+        console.log("The number selected is: " + numb)
+    };
+    if( specialC === true) {
+        var special = specialCharEl[Math.floor(Math.random() * specialCharEl.length)]
+        console.log("The special character selected is: " + special)
+    };
+    return starterPW
+}
+    // Starter PW
+    var starterPW = upper + lower + numb + special
+    console.log(starterPW) 
     
     console.log("upperCases: " + pwCharEl[0])
     console.log("lowerCases: " + pwCharEl[1])
@@ -58,8 +87,7 @@ function generatePassword() {
 
 
     var pw = 'User password here';
-    return pw;
-};
+
 
 // Write password to the #password input
 function writePassword() {
