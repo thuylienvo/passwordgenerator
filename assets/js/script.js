a8291e4b43b1c2fa0578780e208695f7d8fe50ac
Mimi Vo <mimzy414@gmail.com>
2021-04-08 00:02:55 -0600

    updated window prompt for pwlength

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,11 @@
 
 // Prompts after user clicks Generate Password
 // ================================================================
-// Prompt for length of password between 8 and 128 characters
 var generateBtn = document.querySelector("#generate");
    
-
+// Prompt for length of password between 8 and 128 characters
 function capturePwLength() {
-    var pwLength = window.prompt("Please enter a number of characters between 8 and 128.")
+    var pwLength = window.prompt("Please enter the number of characters you would like for your password. Must between 8 and 128. Must have at least 1 character selection.")
     console.log(pwLength)
     if(pwLength % 1 != 0 || pwLength <8 || pwLength >128) {
         window.alert("Error: You must input a whole number between 8 and 128.")
@@ -19,16 +18,20 @@ function capturePwLength() {
 }
 
 //Prompt for character property types 
-
 function capturePwCharType() { 
-    var upperCases = confirm("Do you want uppercase letters?")
+    var upperCases = confirm("Do you want uppercase letters in your password?")
     console.log(upperCases)
-    var lowerCases = confirm("Do you want lowercase letters?")
+    var lowerCases = confirm("Do you want lowercase letters in your password??")
     console.log(lowerCases)
-    var numbers = confirm("Do you want numbers?")
+    var numbers = confirm("Do you want numbers in your password??")
     console.log(numbers)
-    var special = confirm("Do you want special characters?")
+    var special = confirm("Do you want special characters in your password??")
     console.log(special)
+    if(upperCases & lowerCases & numbers & special === false) {
+        window.alert("Error: You must select at least 1 character type for your password.")
+        pwCharType = capturePwCharType();
+    };
+    return pwCharType
 }
 
 
@@ -40,7 +43,7 @@ function generatePassword() {
     var pwLengthEl =  capturePwLength(); 
 
     // User input character properties
-    var pwCharac = 
+    var pwCharEl = capturePwCharType();
 
 
     // Generate password based on criteria selected
